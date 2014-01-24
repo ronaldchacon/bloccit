e703a287741dbe4151237de0db66f9f49f12ddfe
Ronald Chacon <ronald.chacon87@gmail.com>
2014-01-23 22:34:20 -0800

    Associated and nested topics and posts.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
-  attr_accessible :body, :title
+  attr_accessible :body, :title, :topic
+  belongs_to :topic
 
   default_scope order('created_at DESC')
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
 require 'faker'
 
+# Create 15 topics
+topics = []
+15.times do
+  topics << Topic.create(
+    name: Faker::Lorem.words(rand(1..10)).join(" "),
+    description: Faker::Lorem.paragraph(rand(1..4))
+  )
+end
 rand(4..10).times do
   password = Faker::Lorem.characters(10)
   u = User.new(
@@ -16,12 +24,17 @@ rand(4..10).times do
   # to avoid sending an email. The `save` method updates the database.
 
   rand(5..12).times do
+    topic = topics.first # getting the first topic here
     p = u.posts.create(
+      topic: topic,
       title: Faker::Lorem.words(rand(1..10)).join(" "),
       body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
     # set the created_at to a time within the past year
     p.update_attribute(:created_at, Time.now - rand(600..31536000))
 
+    topics.rotate! # add this line to move the first topic to the last, so that posts get assigned to different topics.
+
+    # comments...
     rand(3..7).times do
       p.comments.create(
         body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"))
@@ -29,10 +42,31 @@ rand(4..10).times do
   end
 end
 
+u = User.new(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'admin')
 
-u = User.first
-u.skip_reconfirmation!
-u.update_attributes(email: 'ronald.chacon87@gmail.com', password: 'asdf1234', password_confirmation: 'asdf1234')
+u = User.new(
+  name: 'Moderator User',
+  email: 'moderator@example.com',
+  password: 'helloworld',
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'moderator')
+
+u = User.new(
+  name: 'Member User',
+  email: 'member@example.com',
+  password: 'helloworld',
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
 
 puts "Seed finished"
 puts "#{User.count} users created"
