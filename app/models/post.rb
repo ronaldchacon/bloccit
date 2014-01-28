cdf9751d166d3123a1c7b0639d3ffc9769a2351f
Ronald Chacon <ronald.chacon87@gmail.com>
2014-01-27 21:22:19 -0800

    Implemented favoriting

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   belongs_to :user
   has_many :votes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   attr_accessible :body, :title, :topic, :image
   belongs_to :topic
   mount_uploader :image, ImageUploader
